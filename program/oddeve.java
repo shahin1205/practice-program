629a0e25fa8355f5dfbcab455e398bfcc9d61a30
Mohammed shahin ali <116484200+shahin1205@users.noreply.github.com>
2022-10-27 14:07:42 +0530

    Add files via upload

---
+++ program/oddeve.java
@@ -0,0 +1,24 @@
+/*
+Name : Mohammed shahin Ali
+date : 27-10-2022
+Question : odd and even
+
+*/class even
+ {
+public static void main(String arge[])
+{
+	System.out.println("The even numbers");
+	for(int i=1; i<=100; i++)
+	{
+		if(i%2==0)
+			System.out.print(" "+i);
+	}
+	System.out.println();
+	System.out.println("The odd numbers");
+for(int i=1; i<=100; i++){
+		if(i%2==1)
+			System.out.print(" "+i);		
+}				
+}
+}
+
