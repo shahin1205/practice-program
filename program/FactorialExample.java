89165e4921bd52f8daeb4a359b1a5d9346b61b86
Mohammed shahin ali <116484200+shahin1205@users.noreply.github.com>
2022-10-27 16:24:13 +0530

    Add files via upload

---
+++ program/FactorialExample.java
@@ -0,0 +1,16 @@
+/*
+Name : Mohammed shahin Ali
+date : 27-10-2022
+Question : factoril
+
+*/
+class FactorialExample{  
+	 public static void main(String args[]){  
+	  int i,fact=1;  
+	  int number=5;    
+	  for(i=1;i<=number;i++){    
+	      fact=fact*i;    
+	  }    
+	  System.out.println("Factorial of "+number+" is: "+fact);
+	  }
+}
